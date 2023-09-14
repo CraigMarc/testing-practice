b275591f0f393d94513d1b3197078ecc7cef5cce
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-14 13:12:17 -0400

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import './styles.css';
 
-function sum(a, b) {
-    return a + b;
-  }
-
-  
- 
-  
-
-
-
-  module.exports = sum;
- // export default sum;
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,6 @@
 //const sum = require('./sum');
-import sum from './index';
+import sum from './function';
+console.log(sum(1,2))
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
