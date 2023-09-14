edb2853ef85bb6a0c25cd86ecfa98562ea1c3c7d
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-14 14:42:11 -0400

    added tests

--- src/index.js
+++ src/index.js
@@ -2,31 +2,31 @@
 
 function sum(a, b) {
     return a + b;
-  }
+}
 
-  function capitalize(string) {
-    
+function capitalize(string) {
+
     return string[0].toUpperCase() + string.slice(1)
-    
-  }
-  
- 
-  function reverse(string) {
-   
+
+}
+
+
+function reverse(string) {
+
     let arr = []
 
     for (let i = string.length; i >= 0; i--) {
         arr.push(string[i])
     }
     return arr.join("")
-  }
+}
 
-  
 
-  
-  //export default sum;
-  export {
-   sum,
-   capitalize,
-   reverse,
-  };
+
+
+//export default sum;
+export {
+    sum,
+    capitalize,
+    reverse,
+};
--- src/sum.test.js
+++ src/sum.test.js
@@ -8,15 +8,14 @@ import {
 
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-  
+  expect(sum(1, 2)).toBe(3);
+});
 
-  test('check if first letter is capital', () => {
-    expect(capitalize('hello')).toBe('Hello');
-  });
+test('check if first letter is capital', () => {
+  expect(capitalize('hello')).toBe('Hello');
+});
 
-  test('check if string is reversed', () => {
-    expect(reverse('real')).toBe('laer');
-  });
+test('check if string is reversed', () => {
+  expect(reverse('real')).toBe('laer');
+});
 
